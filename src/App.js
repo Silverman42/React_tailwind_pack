ee2100c1d1ae216db38835c79e1618f91b561925
silverman42 <snkeze9@gmail.com>
2020-02-13 16:17:13 +0100

    Added: React configured with Tailwind configuration for starter projects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className="bg-blue-500 py-30 min-h-screen">
+      <header className="w-10/12 mx-auto">
+        <img src={logo} className="w-32 h-auto mb-5 block mx-auto" alt="logo" />
+        <p className="text-center text-2xl text-white">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
